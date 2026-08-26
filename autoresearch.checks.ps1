a1e825f57922c2ec015e18e2b35238205af23d7b
Matej Scherner <m.scherner@rey-betonwerk.de>
2026-08-26 08:29:26 +0200

    chore: fix solver research checks

--- autoresearch.checks.ps1
+++ autoresearch.checks.ps1
@@ -2,5 +2,5 @@ $ErrorActionPreference = "Stop"
 
 # Add correctness checks here. Keep success output quiet and failures actionable.
 $global:LASTEXITCODE = 0
-node --test \
+node --test test/chemistry.test.js test/solver-constraint-coverage.test.js test/solver-policy-adapter.test.js test/solver-policy-fodder.test.js test/solver-policy-identity.test.js test/solver-policy-rating.test.js test/solver-policy-target-project.test.js test/solver-regressions.test.js
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
